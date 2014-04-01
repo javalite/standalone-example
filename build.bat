f1193ec0ec18b6f4539a715b7dcdc300ade8faca
Igor Polevoy <igor@expresspigeon.com>
2014-03-31 20:09:10 -0500

    Please update the .bat files #1

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 @echo off
 del /S /F /Q classes
 
-set CLASSPATH=lib\activejdbc-1.4.1.jar
-set CLASSPATH=%CLASSPATH%;lib\javalite-common-1.4.1.jar
+set CLASSPATH=lib\activejdbc-1.4.6.jar
+set CLASSPATH=%CLASSPATH%;lib\javalite-common-1.4.6.jar
 set CLASSPATH=%CLASSPATH%;lib\mysql-connector-java-5.0.4.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-api-1.5.10.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-simple-1.5.10.jar
