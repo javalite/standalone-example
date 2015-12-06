82eb26d5eff6cd49340629046005fa41a34d64cd
Igor Polevoy <igor@expresspigeon.com>
2015-12-06 14:13:38 -0600

    upgrade to new version

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 
 set CLASSPATH=classes
-set CLASSPATH=%CLASSPATH%;lib\activejdbc-1.4.1.jar
-set CLASSPATH=%CLASSPATH%;lib\javalite-common-1.4.1.jar
+set CLASSPATH=%CLASSPATH%;lib\activejdbc-1.4.11.jar
+set CLASSPATH=%CLASSPATH%;lib\javalite-common-1.4.11.jar
 set CLASSPATH=%CLASSPATH%;lib\mysql-connector-java-5.0.4.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-api-1.5.10.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-simple-1.5.10.jar
